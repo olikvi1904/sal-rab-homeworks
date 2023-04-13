26b5fd6230c560fe8feba8bd05337216a471d160
Olik <olikvi1904@mail.ru>
2023-04-13 21:51:22 +0300

    commit2

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,9 +3,9 @@
 
         function parseProducts(json) {
         let data = JSON.parse(json);
+        let products = data.products;
         return data.products;
         }
-        
         function renderProductsCards(json) {
         clearProducts();
         let products = parseProducts(json);
